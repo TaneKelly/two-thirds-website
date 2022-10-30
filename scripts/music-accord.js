00ccf9a3289f7d1b24ac9bccf1e75727dd8009b9
Tane Kelly <tane.kelly14@gmail.com>
2022-10-30 16:18:10 +1300

    more border radius to smooth design

---
+++ scripts/music-accord.js
@@ -0,0 +1,6 @@
+var modSizedRoomBtn = document.querySelector(".moderate_sized_room-btn");
+console.log(modSizedRoomBtn);
+
+modSizedRoomBtn.addEventListener("click", function () {
+	console.log("hi");
+});
