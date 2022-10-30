7f741126ab5c6fc731508e50aa3e9a5420f61a36
Tane Kelly <tane.kelly14@gmail.com>
2022-10-30 16:26:16 +1300

    removed unused files

--- scripts/music-accord.js
+++
@@ -1,6 +0,0 @@
-var modSizedRoomBtn = document.querySelector(".moderate_sized_room-btn");
-console.log(modSizedRoomBtn);
-
-modSizedRoomBtn.addEventListener("click", function () {
-	console.log("hi");
-});
